a0b9de41a99edde6a711a3a0b7ae04b2ae593687
tocomi <tocomi0112@gmail.com>
2018-01-08 01:44:45 +0900

    fix bug

--- yukicoder/0345_minimum-chiwawa/ruby/minimum_chiwawa.rb
+++ yukicoder/0345_minimum-chiwawa/ruby/minimum_chiwawa.rb
@@ -1,11 +1,12 @@
 input = gets
-cww_idx = []
 ans = -1
 
 input.each_char.each_with_index do |char, index|
   next unless char == 'c'
 
+  cww_idx = []
   cww_idx.push(index)
+  
   input.each_char.each_with_index do |inner_char, inner_index|
     next if inner_index <= index
     cww_idx.push(inner_index) if inner_char == 'w'
@@ -16,7 +17,6 @@ input.each_char.each_with_index do |char, index|
 
   if (ans == -1 || ans > (cww_idx.last - cww_idx.first) + 1) then
     ans = (cww_idx.last - cww_idx.first) + 1
-    cww_idx = []
   end
 end
 
