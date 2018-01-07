54bbf2276b431c64f7da2e5cdd88e2e042ef7c47
tocomi <tocomi0112@gmail.com>
2018-01-08 01:41:29 +0900

    fix bug

--- yukicoder/0345_minimum-chiwawa/ruby/minimum_chiwawa.rb
+++ yukicoder/0345_minimum-chiwawa/ruby/minimum_chiwawa.rb
@@ -1,18 +1,20 @@
 input = gets
 cww_idx = []
-target = ['c', 'w', 'w']
 ans = -1
 
 input.each_char.each_with_index do |char, index|
-  # when already 'c' found, overwrite index
-  if char == 'c' && cww_idx.length > 0 then
-    cww_idx = [index]
-    next
+  next unless char == 'c'
+
+  cww_idx.push(index)
+  input.each_char.each_with_index do |inner_char, inner_index|
+    next if inner_index <= index
+    cww_idx.push(inner_index) if inner_char == 'w'
+    break if cww_idx.length >= 3
   end
 
-  cww_idx.push(index) if char == target[cww_idx.length]
-  
-  if cww_idx.length == 3 && (ans == -1 || ans > (cww_idx.last - cww_idx.first) + 1) then
+  break if cww_idx.length < 3
+
+  if (ans == -1 || ans > (cww_idx.last - cww_idx.first) + 1) then
     ans = (cww_idx.last - cww_idx.first) + 1
     cww_idx = []
   end
