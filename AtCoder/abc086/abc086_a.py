61453a5327d147a5a01e9da18e81b1e4ddcb233b
tocomi <tocomi0112@gmail.com>
2018-03-18 02:59:02 +0900

    fix: modify input

--- AtCoder/abc086/abc086_a.py
+++ AtCoder/abc086/abc086_a.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import sys
-
 def main():
-    args = raw_input("type 2 numbers with space: ").split()
+    args = input().split()
     a, b = int(args[0]) ,int(args[1])
     result = 'Even' if ( a * b ) % 2 == 0 else 'Odd'
     print(result)
